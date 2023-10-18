a46c8bd70a744755ef5b7325594d2fc2965dcd75
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-10-18 14:12:21 -0300

    Add files via upload

--- aula01/exp01.php
+++ aula01/exp01.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <body>
     <?php 
-    
-    echo "prefira ser o burro entre os intelingentes";
-    
+//isso é um teste no codigo com o github
+    echo "<h1>prefira ser o burro entre os intelingentes</h1>";
+    $number = 12;
+    $resposta = "";
+    if($number > 0):
+$resposta = " o numero é positivo";
+elseif($number < 0 ):
+    $resposta = "o numero é negativo";
+else:
+$resposta = "o numero é igual zero";
+endif;
+echo $resposta;
     ?>
 </body>
 </html>
