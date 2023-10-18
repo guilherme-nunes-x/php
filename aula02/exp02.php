46ab5c4c2a539da7bc256e4bf8b09f3a49d52d86
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-10-18 17:28:54 -0300

    Add files via upload

---
+++ aula02/exp02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="exp02.css">
+    <title>Document</title>
+</head>
+<body>
+<h1>titulo primario</h1>
+<p class="estilo">classe 01</p>
+
+<p>de fora</p>
+<div class = "conteudo">
+
+<p>titulo da div</p>
+<p id = "txt">texto subconteudo</p>
+
+</div>
+
+
+</body>
+</html>
