6bc1c972c7c2e276b127e4965c3ab06e6a964677
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-11-07 14:47:14 -0300

    Add files via upload

---
+++ aula03/exp03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp03.css">
+</head>
+<body>
+
+<p>elemento paragrafo(p)</p>    
+<h1> elemento titulo(h1)</h1>
+<h2> elemento subtitulo(h2)</h2>
+<p id="fazer"> elemento com id</p>
+
+<div class = "star"> div 1</div>
+<div class = "star"> div 2</div>
+<p class = "star"> paragrafo 1</p>
+<h1 class = "star"> titulo</h1>
+
+</body>
+</html>
