2c7b2250ad786e322c25e2a0cc844509e7234479
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-11-07 15:18:41 -0300

    Add files via upload

---
+++ aula03/exp04.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp04.css">
+<style>
+    p{
+        background-color:green;
+    }
+</style>
+</head>
+<body>
+<p id="fazer"> paragrafo 1</p>
+<p style ="background-color: pink;"> paragrafo 1</p>
+<p > paragrafo 1</p>
+<p> paragrafo 1</p>
+
+
+</body>
+</html>
