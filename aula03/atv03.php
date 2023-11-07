1235f1871b3106b8ae3af5295ddacacc39978590
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-11-07 20:59:58 -0300

    Add files via upload

---
+++ aula03/atv03.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="atv03.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+<div id="color1">rgb(196, 104, 181)</div>
+<div id="color2">rgb(104, 196, 196)</div>
+<div id="color3">rgb(196, 104, 135)</div>
+<div id="color4">rgb(90, 10, 83)</div>
+<div id="color5">rgb(218, 230, 45)</div>
+<div id="color6">rgb(59, 182, 48)</div>
+    </div>
+
+</body>
+</html>
