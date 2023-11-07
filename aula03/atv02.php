3d50f5430bca6e009fa6ad44d47f5b521e65cd03
Guilherme nunes <140085055+guilherme-nunes-x@users.noreply.github.com>
2023-11-07 14:06:14 -0300

    Add files via upload

---
+++ aula03/atv02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="atv02.css">
+    <title>Document</title>
+</head>
+<body>
+ 
+<div id="p1">
+<div id="pc1"><p>black</p></div>
+</div>
+<div id="p2"></div>
+
+<div id="p3"></div>
+
+</body>
+</html>
